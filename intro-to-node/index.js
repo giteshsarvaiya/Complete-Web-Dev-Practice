a5b16d956b4a8d7009e4cc6452a07bad10bc5188
Gitesh <gitesh.sarvaiya28@gmail.com>
2023-03-24 07:45:24 +0530

    intro to node added

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var mySuperHero = superheroes.random();
+console.log("mySuperHero " + mySuperHero);
+
+var supervillains = require("supervillains");
+var mySuperVillain = supervillains.random();
+console.log("mySuperVillain " + mySuperVillain);
