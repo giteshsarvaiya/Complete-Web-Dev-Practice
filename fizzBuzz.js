a408b5c89ed74fb3d318a951afde6eba175b6c59
Gitesh <gitesh.sarvaiya28@gmail.com>
2023-04-09 21:49:26 +0530

    added fizzBuzz solution

---
+++ fizzBuzz.js
@@ -0,0 +1,65 @@
+var output = [];
+var count = 1;
+function fizzBuzz() {
+    if(count%3===0){
+        if (count%15===0){
+            output.push("fizzbuzz");
+        } else {
+            output.push("fizz");
+        }
+    }  else if (count%5===0) {
+        output.push("buzz");
+    } else {
+        output.push(count);
+    }
+count++
+    console.log(output);
+}
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
